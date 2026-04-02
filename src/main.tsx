7826992bef86743466d55dc9baf37c07130bac63
JoMi2026 <jomi5436@gmail.com>
2026-04-02 18:55:13 +0200

    add files

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from './pages/Home/Home.tsx';
 import Bounty from './pages/Bounty/Bounty.tsx'
+import Mission from "./pages/Mission/Mission.tsx";
+import Profil from "./pages/Profil/Profil.tsx";
 import './index.css'
 import App from './App.tsx'
 
@@ -21,6 +23,10 @@ const router = createBrowserRouter([
         path:"/bounty",
         element: <Bounty/>
       },
+      {
+        path:"/profil",
+        element: <Profil />
+      }
       
     ],
   },
--- src/pages/Profil/Profil.tsx
+++ src/pages/Profil/Profil.tsx
@@ -1,13 +1,9 @@
 import ProfilDescriptif from "../../components/ProfilDescriptif/ProfilDescriptif";
 
 function Profil() {
-<<<<<<< HEAD
 	return <>
 	<ProfilDescriptif />
 	</>;
-=======
-	return <></>;
->>>>>>> f7d2c73386158692efe97e1c316d41df774f9e15
 }
 
 export default Profil;
