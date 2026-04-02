e024ad4c678c37f40b79df6655ba2e7a4f1ec616
Leo Fleury <leofleury72@gmail.com>
2026-04-02 16:28:29 +0200

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,25 @@
+import "./Card.css";
+function Card() {
+	return (
+		<section className="card-section">
+			<div className="card-container">
+				<div className="container-img-info">
+					<div className="card-div-img">
+						<img src="" alt="" />
+					</div>
+					<div className="card-div-info">
+						<p></p>
+						<p></p>
+						<p></p>
+						<p></p>
+					</div>
+				</div>
+				<div className="card-div-description">
+					<p></p>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default Card;
