d6f58a8e92796bab4abf8437326b30391e062268
JoMi2026 <jomi5436@gmail.com>
2026-04-02 22:18:09 +0200

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer/Footer"
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ const App = () => {
 		<>
 			<Navbar />
 			<Outlet />
+      <Footer />
 		</>
 	);
 };
