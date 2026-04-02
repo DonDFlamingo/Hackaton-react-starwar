807de3bc1deab7adcc69bb423cea1258857255f3
JoMi2026 <jomi5436@gmail.com>
2026-04-02 17:43:05 +0200

    add files

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/mission",
+        element: <Mission />
+      },
+      {
         path:"/bounty",
         element: <Bounty/>
       },
