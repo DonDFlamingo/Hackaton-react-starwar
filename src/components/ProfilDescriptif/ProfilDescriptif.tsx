375ab17f028f74e38e3c5b336c417eb8c2b165c1
Brice Kutuk <bricekutuk@live.fr>
2026-04-02 18:04:38 +0200

    the top page of the profil page

--- src/components/ProfilDescriptif/ProfilDescriptif.tsx
+++ src/components/ProfilDescriptif/ProfilDescriptif.tsx
@@ -10,36 +10,36 @@ function ProfilDescriptif() {
             <div className='Compétance'>
 
                 <div className='div1'>
-                    <h2 >DOSSIER :</h2>
+                    <h2 >DOSSIER </h2>
                 </div>
 
                 <div className='div2'>
-                    <p className='titreCompétance'>Nom de code : </p> 
+                    <p className='titreCompétance'>Nom de code </p> 
                     <p className='paraCompétance'> Abdou Ba Fett </p>
                 </div>
                 
                 <div className='div3'>
-                    <p className='titreCompétance'>Taille : </p>
+                    <p className='titreCompétance'>Taille </p>
                     <p className='paraCompétance'>1.83m</p>
                 </div>           
 
                 <div className='div4'>
-                    <p className='titreCompétance'>Genre : </p>
+                    <p className='titreCompétance'>Genre  </p>
                     <p className='paraCompétance'> Homme.</p>
                 </div>
 
                 <div className='div5'>
-                    <p className='titreCompétance'>Homeworld : </p>
+                    <p className='titreCompétance'>Homeworld  </p>
                     <p className='paraCompétance'> Kamino.</p>
                 </div>
 
                 <div className='div6'>
-                    <p className='titreCompétance'>SkinColor : </p>
+                    <p className='titreCompétance'>SkinColor  </p>
                     <p className='paraCompétance'> Tan.</p>
                 </div>
 
                 <div className='div7'>
-                    <p className='titreCompétance'>Species :</p>
+                    <p className='titreCompétance'>Species </p>
                     <p className='paraCompétance'>Human</p>
                 </div>
             </div>
