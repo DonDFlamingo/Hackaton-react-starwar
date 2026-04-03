8471aa35d6e08e34d08665f25a4ab21b76538097
Brice Kutuk <bricekutuk@live.fr>
2026-04-03 12:01:41 +0200

    modif of the css descriptif for the profil page

--- src/pages/Profil/Profil.tsx
+++ src/pages/Profil/Profil.tsx
@@ -2,19 +2,12 @@ import ProfilDescriptif from "../../components/ProfilDescriptif/ProfilDescriptif
 import ProfilMissionStats from "../../components/ProfilMissionStats/ProfilMissionStats";
 
 function Profil() {
-<<<<<<< HEAD
-	return <>
-	<ProfilDescriptif />
-	<ProfilMissionStats />
-	</>;
-=======
 	return (
 		<>
 			<ProfilDescriptif />
 			<ProfilMissionStats />
 		</>
 	);
->>>>>>> 608585a43ad2a52a0841d672c4e7345e6333822d
 }
 
 export default Profil;
